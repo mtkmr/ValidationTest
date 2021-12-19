2e00af699b8201a0422314d519faffabc7da77b0
mtkmr <m.takamura0520@gmail.com>
2021-12-19 15:35:01 +0900

    Validator名の変更に伴うテストの修正

--- ValidationTestTests/ValidatorTest.swift
+++ ValidationTestTests/ValidatorTest.swift
@@ -21,19 +21,19 @@ class ValidatorTest: XCTestCase {
     }
 
     func testNameValidator() {
-        let nameValidator = NameCompositeValidator()
+        let validator = CompositeValidatorFactory.shared.nameValidator()
         let nameValid = "田中太郎"
         let nameEmpty = ""
         let nameLengthOver20 = "長い氏名長い氏名長い氏名長い氏名長い氏名長い氏名"
         let nameFalseFormat = "korehahankakudesu"
-        XCTAssertNoThrow(try isValid(nameValidator.validate(nameValid)), "成功ケースを正常に検出できていません")
-        XCTAssertThrowsError(try isValid(nameValidator.validate(nameEmpty)), "エラーが投げられていません。") { error in
+        XCTAssertNoThrow(try isValid(validator.validate(nameValid)), "成功ケースを正常に検出できていません")
+        XCTAssertThrowsError(try isValid(validator.validate(nameEmpty)), "エラーが投げられていません。") { error in
             XCTAssertEqual(error as! ValidationError, ValidationError.empty, "ValdationError.emptyが投げられていません")
         }
-        XCTAssertThrowsError(try isValid(nameValidator.validate(nameLengthOver20)), "エラーが投げられていません。") { error in
+        XCTAssertThrowsError(try isValid(validator.validate(nameLengthOver20)), "エラーが投げられていません。") { error in
             XCTAssertEqual(error as! ValidationError, ValidationError.length(min: 1, max: 20), "ValdationError.length(min: 1, max: 20)が投げられていません")
         }
-        XCTAssertThrowsError(try isValid(nameValidator.validate(nameFalseFormat)), "エラーが投げられていません。") { error in
+        XCTAssertThrowsError(try isValid(validator.validate(nameFalseFormat)), "エラーが投げられていません。") { error in
             XCTAssertEqual(error as! ValidationError, ValidationError.notFullWidth, "ValdationError.fullWidthが投げられていません")
         }
     }
